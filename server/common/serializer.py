1f930f877e811e4c65d46aa17b396a2e399c6f80
ivanovic99 <ivanerlich@gmail.com>
2024-08-29 20:15:54 -0300

    Update: Using utils. Bet

--- server/common/serializer.py
+++ server/common/serializer.py
@@ -1,34 +1,8 @@
 import json
+from .utils import Bet
 
-class Bet:
-    def __init__(self, nombre, apellido, dni, nacimiento, numero):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.dni = dni
-        self.nacimiento = nacimiento
-        self.numero = numero
-
-    def to_dict(self):
-        return {
-            "nombre": self.nombre,
-            "apellido": self.apellido,
-            "dni": self.dni,
-            "nacimiento": self.nacimiento,
-            "numero": self.numero
-        }
-
-    @staticmethod
-    def from_dict(data):
-        return Bet(
-            nombre=data["nombre"],
-            apellido=data["apellido"],
-            dni=data["dni"],
-            nacimiento=data["nacimiento"],
-            numero=data["numero"]
-        )
-
-def serialize_bet(bet):
+def serialize_bet(bet: Bet) -> bytes:
     return json.dumps(bet.to_dict()).encode('utf-8')
 
-def deserialize_bet(data):
+def deserialize_bet(data: bytes) -> Bet:
     return Bet.from_dict(json.loads(data.decode('utf-8')))
