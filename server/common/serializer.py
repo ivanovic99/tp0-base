3c38e62ac0ab948654bcef256d0f68e2f0f9bdd6
ivanovic99 <ivanerlich@gmail.com>
2024-08-31 18:59:25 -0300

    Update: deserialize multiple bets

--- server/common/serializer.py
+++ server/common/serializer.py
@@ -1,8 +1,5 @@
 import json
 from .utils import Bet
 
-def serialize_bet(bet: Bet) -> bytes:
-    return json.dumps(bet.to_dict()).encode('utf-8')
-
-def deserialize_bet(data: bytes) -> Bet:
-    return Bet.from_dict(json.loads(data.decode('utf-8')))
+def deserialize_bets(data: bytes) -> list[Bet]:
+    return [Bet.from_dict(bet) for bet in json.loads(data.decode('utf-8'))]
