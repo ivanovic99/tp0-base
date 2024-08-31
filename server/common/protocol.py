b88983e3bf7fba16072e4ed224b51e6ca1919ace
ivanovic99 <ivanerlich@gmail.com>
2024-08-31 19:00:17 -0300

    Update: receiving multiple bets (batch), total of bets and sending ok (confirmation to client) methods

--- server/common/protocol.py
+++ server/common/protocol.py
@@ -1,26 +1,44 @@
-from .serializer import serialize_bet, deserialize_bet
 import struct
+from .serializer import deserialize_bets
 
-AMMOUNT_OF_BYTES = 4
+AMOUNT_OF_BYTES = 4
 
 class Protocol:
     def __init__(self, conn):
         self.conn = conn
 
-    def send_bet(self, bet):
-        data = serialize_bet(bet)
-        self.conn.sendall(data + b'\n')
-
-    def receive_bet(self):
-        length_data = self.conn.recv(AMMOUNT_OF_BYTES)
-        if len(length_data) < AMMOUNT_OF_BYTES:
+    def receive_bets(self):
+        """Receive a list of bets from the client."""
+        length_data = self._recv_n_bytes(AMOUNT_OF_BYTES)
+        if len(length_data) < AMOUNT_OF_BYTES:
             raise ValueError("Incomplete length data received")
 
         length = struct.unpack('!I', length_data)[0]
 
-        data = self.conn.recv(length)
+        data = self._recv_n_bytes(length)
         if len(data) < length:
-            raise ValueError("Incomplete bet data received")
+            raise ValueError("Incomplete bets data received")
 
-        bet = deserialize_bet(data)
-        return bet
+        bets = deserialize_bets(data)
+        return bets
+
+    def _recv_n_bytes(self, n):
+        """Helper method to receive exactly n bytes or raise an error."""
+        data = bytearray()
+        while len(data) < n:
+            packet = self.conn.recv(n - len(data))
+            if not packet:
+                raise ValueError("Connection closed before receiving all data")
+            data.extend(packet)
+        return bytes(data)
+
+    def receive_total_bets(self):
+        total_bets_bytes = self.conn.recv(AMOUNT_OF_BYTES)
+        if len(total_bets_bytes) < AMOUNT_OF_BYTES:
+            raise ValueError("Failed to receive total bets")
+        total_bets = int.from_bytes(total_bets_bytes, byteorder='big')
+        return total_bets
+    
+    def send_ok(self, ok):
+        data = b'\x01' if ok else b'\x00'
+        self.conn.sendall(data)
