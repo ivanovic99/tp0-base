9295a61b918cfcd83d1f285269b414db2160101b
ivanovic99 <ivanerlich@gmail.com>
2024-08-31 18:58:32 -0300

    Update: sending batch of Bets, Sending total AmountOfBets and ReceivingOk (confirmation from server)

--- client/common/protocol.go
+++ client/common/protocol.go
@@ -10,19 +10,20 @@ type Protocol struct {
     conn net.Conn
 }
 
+const AMMOUNT_OF_BYTES = 4
+
 func NewProtocol(conn net.Conn) *Protocol {
     return &Protocol{conn: conn}
 }
 
-func (protocol *Protocol) SendBet(bet Bet) error {
-
-    data, err := SerializeBet(bet)
+func (protocol *Protocol) SendBets(bets []Bet) error {
+    data, err := SerializeBets(bets)
     if err != nil {
         return err
     }
 
     length := uint32(len(data))
-    lengthBuf := make([]byte, 4)
+    lengthBuf := make([]byte, AMMOUNT_OF_BYTES)
     binary.BigEndian.PutUint32(lengthBuf, length)
 
     _, err = protocol.conn.Write(lengthBuf)
@@ -36,4 +37,28 @@ func (protocol *Protocol) SendBet(bet Bet) error {
     }
 
     return nil
+}
+
+func (protocol *Protocol) AmountOfBets(totalBets uint32) error {
+    // Convert the total number of bets to a byte slice
+    totalBetsBytes := make([]byte, AMMOUNT_OF_BYTES)
+    binary.BigEndian.PutUint32(totalBetsBytes, totalBets)
+    
+    // Send the byte slice over the connection
+    _, err := protocol.conn.Write(totalBetsBytes)
+    return err
+}
+
+func (protocol *Protocol) ReceiveOk() (bool, error) {
+    // Read the response from the server, only one byte
+    response := make([]byte, 1)
+    _, err := protocol.conn.Read(response)
+    if err != nil {
+        return false, err
+    }
+    if response[0] == 0x01 {
+        return true, nil
+    } else {
+        return false, nil
+    }
 }
