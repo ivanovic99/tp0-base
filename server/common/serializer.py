2ec6898271a10ae70c2719fb44fe7325765bd2a4
ivanovic99 <ivanerlich@gmail.com>
2024-08-31 22:33:19 -0300

    Serializing winners

--- server/common/serializer.py
+++ server/common/serializer.py
@@ -3,3 +3,6 @@ from .utils import Bet
 
 def deserialize_bets(data: bytes) -> list[Bet]:
     return [Bet.from_dict(bet) for bet in json.loads(data.decode('utf-8'))]
+
+def serialize_winners(winners: list[str]) -> bytes:
+    return json.dumps(winners).encode('utf-8')
