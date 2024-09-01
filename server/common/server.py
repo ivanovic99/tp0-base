67669f9f7b5fc738368d0506295937704a67eb5e
ivanovic99 <ivanerlich@gmail.com>
2024-08-31 22:35:17 -0300

    Server using Barrier now. Udjusted the server for the new protocol and we are performing the draw

--- server/common/server.py
+++ server/common/server.py
@@ -2,7 +2,9 @@ import socket
 import logging
 import threading
 from .protocol import Protocol
-from .utils import store_bets
+from .utils import store_bets, load_bets, has_won
+
+TOTAL_CLIENTS = 2
 
 class Server:
     def __init__(self, port, listen_backlog):
@@ -12,6 +14,11 @@ class Server:
         self._server_socket.listen(listen_backlog)
         self._clients = []
         self._is_running = True
+        self._finished_clients = 0
+        self._total_clients = TOTAL_CLIENTS
+        self._winners = {1: [], 2: []}
+        self._barrier = threading.Barrier(self._total_clients)
+
 
     def run(self):
         """
@@ -43,30 +50,56 @@ class Server:
         """
         try:
             protocol = Protocol(client_sock)
-            total_bets = protocol.receive_total_bets()
-            logging.info(f"action: receive_total_bets | result: success | total_bets: {total_bets}")
-
-            received_bets = 0
-            while received_bets < total_bets:
+            has_finished = False
+            while not has_finished:
                 try:
-                    bets = protocol.receive_bets()
-                    if not bets:
-                        break
-                    try:
-                        store_bets(bets)
-                        received_bets += len(bets)
-                        logging.info(f'action: apuesta_recibida | result: success | cantidad: {len(bets)}')
-                    except Exception as e:
-                        logging.info(f'action: apuesta_recibida | result: fail | cantidad: {len(bets)}')
+                    case_id = protocol.receive_case()
+                    if case_id == 1:
+                        bets = protocol.receive_bets()
+                        if not bets:
+                            has_finished = True
+                        try:
+                            store_bets(bets)
+                            logging.info(f'action: apuesta_recibida | result: success | cantidad: {len(bets)}')
+                        except Exception as e:
+                            logging.info(f'action: apuesta_recibida | result: fail | cantidad: {len(bets)}')
+                    elif case_id == 2:
+                        has_finished = True
                 except Exception as e:
                     logging.error("action: receive_message | result: fail | error: {e}")
                     break
-            if received_bets == total_bets:
-                logging.info("action: receive_all_bets | result: success")
-                protocol.send_ok(True)
+            if not has_finished:
+                logging.error("action: receive_message | result: fail | error: has not finished")
+                client_sock.close()
+                return
+            logging.info("action: receive_all_bets | result: success")
+            protocol.send_ok(True)
+
+            self._finished_clients += 1
+            if self._finished_clients == self._total_clients:
+                logging.info("action: sorteo | result: success")
+                self.__perform_draw()
+
+            case_id = protocol.receive_case()
+            if case_id == 3:
+                logging.info(f'action: receive_case | result: success | case_id: {case_id}')
+                agency_id = protocol.receive_agency_id()
+                logging.info(f'action: receive_agency_id | result: success | agency_id: {agency_id}')
+                # Wait at the barrier
+                logging.info(f'action: winners_status_BEFORE_barrier | result: in_progress | winners: {self._winners}')
+                self._barrier.wait()
+                logging.info(f'action: winners_status_AFTER_barrier | result: in_progress | winners: {self._winners}')
+                winners = []
+                if agency_id in self._winners:
+                    winners = self._winners[agency_id]
+                    logging.info(f'action: send_winners | result: in_progress | winners: {winners}')
+                    protocol.send_winners(winners)
+                    logging.info(f'action: winners_sent | result: success')
             else:
-                logging.error("action: receive_all_bets | result: fail | received_bets: {received_bets} | total_bets: {total_bets}")
-                protocol.send_ok(False)
+                logging.error("action: receive_message | result: fail | error: invalid case_id")
+                client_sock.close()
+                return
+            
         finally:
             client_sock.close()
 
@@ -95,4 +128,13 @@ class Server:
             client.join()
         logging.info('action: shutdown | result: all_clients_joined')
 
-
+    def __perform_draw(self):
+        """
+        Perform a draw of the bets
+        """
+        bets = list(load_bets())
+        logging.info(f'action: sorteo | result: in_progress | cantidad: {len(bets)}')
+        for bet in bets:
+            if has_won(bet):
+                logging.info(f'action: sorteo | result: winner | winner_id: {bet.document}')
+                self._winners[bet.agency].append(bet.document)
