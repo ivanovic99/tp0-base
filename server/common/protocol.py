5b74b24967f77d7e1cbcaf34c6f559ef304019b9
ivanovic99 <ivanerlich@gmail.com>
2024-08-31 22:34:25 -0300

    Protocol changed, now we receive each case id and after that the actual case. we also implemented the receive agency id and sending the winners

--- server/common/protocol.py
+++ server/common/protocol.py
@@ -1,11 +1,34 @@
 import struct
-from .serializer import deserialize_bets
+from .serializer import deserialize_bets, serialize_winners
 
 AMOUNT_OF_BYTES = 4
 
 class Protocol:
     def __init__(self, conn):
         self.conn = conn
+
+    def receive_case(self):
+        """Receive a case ID from the client."""
+        data = self._recv_n_bytes(1)
+        if len(data) < 1:
+            raise ValueError("Incomplete case ID received")
+        case_id = ord(data)
+        return case_id
+
+    def receive_agency_id(self):
+        """Receive an agency ID from the client."""
+        data = self._recv_n_bytes(AMOUNT_OF_BYTES)
+        if len(data) < AMOUNT_OF_BYTES:
+            raise ValueError("Incomplete agency ID received")
+        agency_id = struct.unpack('!I', data)[0]
+        return agency_id
+    
+    def send_winners(self, documents_of_winners):
+        """Send the documents of the winners to the client."""
+        data = serialize_winners(documents_of_winners)
+        length = len(data).to_bytes(AMOUNT_OF_BYTES, byteorder='big')
+        self.conn.sendall(length)
+        self.conn.sendall(data)
 
     def receive_bets(self):
         """Receive a list of bets from the client."""
@@ -40,5 +63,5 @@ class Protocol:
         return total_bets
     
     def send_ok(self, ok):
-        data = b'\x01' if ok else b'\x00'
+        data = b'\x02' if ok else b'\x00'
         self.conn.sendall(data)
