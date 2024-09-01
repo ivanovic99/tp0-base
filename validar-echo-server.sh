fbfdeb2c94ecc2d5a6fd3ba9ddb2d3051540c048
ivanovic99 <ivanerlich@gmail.com>
2024-09-01 15:33:15 -0300

    Update: Refactor for ONLY this exercise, so it is cleaner. NOTE: None of the refactors are being passed over the next exercises, each one has its own refactor for the specific code that needs to be implemented

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -9,6 +9,9 @@ NETWORK_NAME="tp0_testing_net"
 # Mensaje de prueba
 TEST_MESSAGE="Hello, Echo Server! I am working fine yeyyyyy"
 
+# Puerto del servidor (puede ser parametrizado si es necesario)
+SERVER_PORT=12345
+
 # Crear un contenedor temporal para realizar la prueba
 docker run --rm --network $NETWORK_NAME alpine sh -c "
     # Instalar netcat
@@ -17,8 +20,14 @@ docker run --rm --network $NETWORK_NAME alpine sh -c "
     # Obtener la IP del servidor
     SERVER_IP=\$(getent hosts $SERVER_CONTAINER_NAME | awk '{ print \$1 }')
 
+    # Verificar si se obtuvo la IP del servidor
+    if [ -z \"\$SERVER_IP\" ]; then
+        echo 'action: test_echo_server | result: fail | error: could not resolve server IP'
+        exit 1
+    fi
+
     # Enviar el mensaje de prueba al servidor y recibir la respuesta
-    RESPONSE=\$(echo $TEST_MESSAGE | nc \$SERVER_IP 12345)
+    RESPONSE=\$(echo $TEST_MESSAGE | nc \$SERVER_IP $SERVER_PORT)
 
     # Verificar si la respuesta es igual al mensaje de prueba
     if [ \"\$RESPONSE\" = \"$TEST_MESSAGE\" ]; then
